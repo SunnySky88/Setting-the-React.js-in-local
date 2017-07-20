b2a9fa4bd3aa0c3cea71f75f00afd66c696bd434
sunil kumar yadav angadala <angadalasunilyadav@gmail.com>
2017-07-19 19:59:46 -0700

    React Installation
    Setting the react.js in local environment

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var config = {
+   entry: './main.js',
+	
+   output: {
+	   path: __dirname,
+      filename: 'index.js',
+   },
+   
+    resolve: {
+    extensions: ['.js', '.jsx']
+  },
+	
+   devServer: {
+      inline: true,
+      port: 8080
+   },
+	
+   module: {
+      loaders: [
+         {
+        test: /\.(js|jsx)$/,
+        loaders: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+         }
+      ]
+   }
+}
+
+module.exports = config;
